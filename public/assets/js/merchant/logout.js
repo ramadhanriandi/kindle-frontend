d9b23ef9f6ef7c8426c1d7496215b8a37d2026a3
ramadhanriandi <izurbudianto@gmail.com>
2019-12-16 17:15:25 +0700

    integrate merchant login api

---
+++ public/assets/js/merchant/logout.js
@@ -0,0 +1,11 @@
+function merchantLogout() {
+  setCookie("", "", -1);
+  location.href = "/login";
+}
+
+function setCookie(email, role, expiredDay) {
+  const date = new Date();
+  date.setTime(date.getTime() + (expiredDay * 24 * 60 * 60 * 1000));
+  const expires = "expires="+ date.toUTCString();
+  document.cookie = "email=" + email + "|" + role + ";" + expires + ";path=/";
+}
