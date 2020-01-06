498c98fb3f655b30b611b5dd7324d7c91c8bea7e
ramadhanriandi <izurbudianto@gmail.com>
2020-01-06 11:47:41 +0700

    fix getBookDetail API integration

--- public/assets/js/user/book-detail.js
+++ public/assets/js/user/book-detail.js
@@ -43,39 +43,35 @@ function convertToCurrency(number) {
 
 function renderBookDetail(book_sku){
   const request = new XMLHttpRequest();
-  const url = `http://localhost:8000/kindle-backend/api/books/${book_sku}/detail`
+  const url = `http://localhost:8000/kindle-backend/api/books/${book_sku}`
 
   request.open("GET", url, true);
   request.onload = function(){
-    if (JSON.parse(request.response)["code"] == 200) {
-      const bookData = JSON.parse(request.response)["bookData"];
-      const merchantData = JSON.parse(request.response)["merchant"];
-      const categoryData = JSON.parse(request.response)["categories"];
-      let categories = '';
+    const bookData = JSON.parse(request.response);
 
-      for (let i = 0; i < categoryData.length; i++) {
-        if (i !== 0) {
-          categories += ", ";
-        }
-        categories += categoryData[i]["name"];
+    const parsedCategories = bookData["categories"].split(';');
+    let categories = '';
+    
+    for (let i = 0; i < parsedCategories.length; i++) {
+      categories += parsedCategories[i];
+      if (i != parsedCategories.length - 1) {
+        categories += ', ';
       }
-  
-      document.getElementById("bookSku").value = bookData["bookSku"];
-      document.getElementById("image").innerHTML = `<img class="book-detail-image" src="${bookData["document"]}" />`;
-      document.getElementById("title").innerHTML = bookData["title"];
-      document.getElementById("author").innerHTML = bookData["author"];
-      document.getElementById("year").innerHTML = bookData["year"];
-      document.getElementById("variant").innerHTML = bookData["variant"];
-      document.getElementById("price").innerHTML = "IDR " + convertToCurrency(bookData["price"]);
-      document.getElementById("description").innerHTML = bookData["description"];
-      document.getElementById("merchant").innerHTML = merchantData;
-      document.getElementById("merchant").onclick = function() {
-        location.href=`/merchants/${bookData['merchantId']}`
-      }
-      document.getElementById("categories").innerHTML = categories;
-    } else {
-      alert(JSON.parse(request.response)["message"]);
-    } 
+    }
+
+    document.getElementById("bookSku").value = bookData["bookSku"];
+    document.getElementById("image").innerHTML = `<img class="book-detail-image" src="${bookData["document"]}" />`;
+    document.getElementById("title").innerHTML = bookData["title"];
+    document.getElementById("author").innerHTML = bookData["author"];
+    document.getElementById("year").innerHTML = bookData["year"];
+    document.getElementById("variant").innerHTML = bookData["variant"];
+    document.getElementById("price").innerHTML = "IDR " + convertToCurrency(bookData["price"]);
+    document.getElementById("description").innerHTML = bookData["description"];
+    document.getElementById("merchant").innerHTML = bookData["merchant"]["fullname"];
+    document.getElementById("merchant").onclick = function() {
+      location.href=`/merchants/${bookData['merchantId']}`
+    }
+    document.getElementById("categories").innerHTML = categories;
   }
   request.send();
 }
