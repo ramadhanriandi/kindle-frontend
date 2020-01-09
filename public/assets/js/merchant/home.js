262f4af2275130c604361dfbf6c3162d8f783675
ardyh <13517062@std.stei.itb.ac.id>
2020-01-09 07:02:33 +0700

    fully integrated home with merchant API

--- public/assets/js/merchant/home.js
+++ public/assets/js/merchant/home.js
@@ -14,7 +14,31 @@ function getMerchantId() {
     return parsedCookie[parsedCookie.length - 1]
 }
 
-function renderBooks(){
+function renderMerchantDetail(){
+    return new Promise( function(resolve, reject){
+        const merchantId = getMerchantId();
+        const request = new XMLHttpRequest();
+        const url = `http://localhost:8000/kindle-backend/api/merchants/${merchantId}`;
+
+        request.open("GET", url, true);
+
+        request.send();
+
+        request.onload = function(){
+            const result = JSON.parse(request.response);
+            
+            document.getElementById("merchant-name").innerHTML = result["username"];
+            document.getElementById("merchant-phone").innerHTML = result["phone"];
+            document.getElementById("merchant-description").innerHTML = result["description"];
+        
+            resolve(true);
+        };
+    });
+}
+
+async function renderBooks(){
+    const hasRenderedMerchantDetail = await renderMerchantDetail();
+
     const merchantId = getMerchantId();
     const listContainer = document.getElementById("list-container");
 
